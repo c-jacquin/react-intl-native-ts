75650bec126b10464aa3a8834f13b432072e8835
Charles Jacquin <charles.jacauin@autistici.org>
2018-01-01 03:46:20 +0100

    chore(type): add typings

---
+++ typings/index.d.ts
@@ -0,0 +1,14 @@
+import { SFC } from 'react'
+import {
+    FormattedMessageProps,
+    FormattedNumberProps,
+    FormattedPluralProps,
+    FormattedTimeProps,
+    FormattedDateProps
+} from '../src/types'
+
+export declare const FormattedDate: SFC<FormattedDateProps>
+export declare const FormattedMessage: SFC<FormattedMessageProps>
+export declare const FormattedNumber: SFC<FormattedNumberProps>
+export declare const FormattedPlural: SFC<FormattedPluralProps>
+export declare const FormattedTime: SFC<FormattedTimeProps>
